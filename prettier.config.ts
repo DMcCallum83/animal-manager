0bfb4b99b02201fee16220364b410b362ff9b8cd
DMcCallum83 <david_mccallum83@yahoo.com>
2025-06-23 20:39:48 +0100

    change configs to ts, install jiti to allow that to work, lint project

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,7 +1,6 @@
-/**
- * @type {import("prettier").Config}
- */
-const config = {
+import { type Config } from "prettier";
+
+const config: Config = {
   arrowParens: "always",
   bracketSpacing: true,
   bracketSameLine: false,
